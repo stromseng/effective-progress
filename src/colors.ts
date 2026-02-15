d8ca0d1f65d24c402f3c4cf067849569a4195c06
Magnus Strømseng <magnus.stromseng@gmail.com>
2026-02-15 21:08:43 +0100

    feat: fun rainbow colorizer

--- src/colors.ts
+++ src/colors.ts
@@ -1,5 +1,5 @@
-import chalk from "chalk";
-import { Context, Layer } from "effect";
+import chalk, { type ChalkInstance } from "chalk";
+import { Context, Effect, Layer } from "effect";
 
 export interface ColorizerService {
   readonly fill: (text: string) => string;
@@ -25,6 +25,35 @@ export class Colorizer extends Context.Tag("stromseng.dev/effective-progress/Col
       spinner: chalk.yellow,
       done: chalk.green,
       failed: chalk.red,
+    }),
+  );
+  static readonly Rainbow = Layer.unwrapEffect(
+    Effect.gen(function* () {
+      let colorNumber = 17;
+      yield* Effect.fork(
+        Effect.gen(function* () {
+          while (true) {
+            colorNumber = (colorNumber + 1) % 256;
+            yield* Effect.sleep("200 millis");
+          }
+        }),
+      );
+      const secondFunc = (chalk: ChalkInstance) => {
+        return (text: string) => chalk.ansi256(colorNumber)(text);
+      };
+
+      return Layer.succeed(
+        Colorizer,
+        Colorizer.of({
+          fill: secondFunc(chalk),
+          empty: secondFunc(chalk.dim),
+          brackets: secondFunc(chalk),
+          percent: secondFunc(chalk.bold),
+          spinner: secondFunc(chalk),
+          done: secondFunc(chalk),
+          failed: secondFunc(chalk),
+        }),
+      );
     }),
   );
 }
