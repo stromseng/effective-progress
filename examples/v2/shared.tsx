12302018a82841dcdbe778cbb528813ff4ec34b2
Magnus Strømseng <magnus.stromseng@gmail.com>
2026-03-11 20:11:27 +0100

    Delete shared.tsx

--- examples/v2/shared.tsx
+++
@@ -1,4 +0,0 @@
-import { defaultRendererv2Columns } from "../../src/renderer/default-columns";
-import { createRendererv2InkRenderer } from "../../src/renderer/ink-renderer";
-
-export const rendererv2Renderer = createRendererv2InkRenderer(defaultRendererv2Columns);
